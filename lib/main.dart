df365e1058b00f7ed216e39537f8d812a07d4e8d
yogasaputra206 <yoga98099@gmail.com>
2026-04-26 18:03:00 +0800

    struktur aplikasi flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'screens/dashboard_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'theme/app_colors.dart';
 
-// setup struktur aplikasi flutter
+// struktur aplikasi flutter
 void main() {
   runApp(const YogaaApp());
 }
