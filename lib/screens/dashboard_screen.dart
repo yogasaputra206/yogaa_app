6fe52a4c5c34930249952138ebca248723667b08
yogasaputra206 <yoga98099@gmail.com>
2026-04-26 18:04:25 +0800

    add halaman dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import '../theme/app_colors.dart';
 import '../widgets/blur_sphere.dart';
 
-// menambahkan halaman dashboard
+// add halaman dashboard
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
